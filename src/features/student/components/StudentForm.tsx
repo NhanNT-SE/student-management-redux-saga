e57f774c05cb29f77f821ea2389ead5ff0f0bceb
NhanNT-SE <nhannt.se1905@gmail.com>
2021-08-06 22:36:21 +0700

    done custom inut field

--- src/features/student/components/StudentForm.tsx
+++ src/features/student/components/StudentForm.tsx
@@ -1,3 +1,5 @@
+import { Box, Button } from '@material-ui/core';
+import InputField from 'components/form-fields/InputField';
 import { Student } from 'models';
 import React from 'react';
 import { useForm } from 'react-hook-form';
@@ -10,5 +12,24 @@ export default function StudentForm({ initialValue, onSubmit }: StudentFormProps
   const { control, handleSubmit } = useForm<Student>({
     defaultValues: initialValue,
   });
-  return <div></div>;
+  const handleFormSubmit = (formValue: Student) => {
+    console.log(formValue);
+  };
+  return (
+    <Box maxWidth={400}>
+      <form onSubmit={handleSubmit(handleFormSubmit)}>
+        {/* FORM FIELDS    */}
+        <InputField name="name" control={control} label="Full name" />
+        <InputField name="age" control={control} label="Age" />
+        <InputField name="mark" control={control} label="Mark" />
+        <InputField name="gender" control={control} label="Gender" />
+        <InputField name="city" control={control} label="City" />
+        <Box mt={3}>
+          <Button type="submit" variant="contained" color="primary">
+            Save
+          </Button>
+        </Box>
+      </form>
+    </Box>
+  );
 }
